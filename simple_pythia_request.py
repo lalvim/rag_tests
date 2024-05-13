8b1e32c9b0b9912a8ccf079bd30580ca12e8e5eb
leandro <alvim.lgm@gmail.com>
2024-05-13 18:41:31 -0300

    Add: normalizer

--- simple_pythia_request.py
+++ simple_pythia_request.py
@@ -1,6 +1,21 @@
 import requests
 import simplejson
 
+import unicodedata
+import re
+import codecs 
+
+def normalize(text):
+
+    clean_text = text.encode('raw_unicode_escape').decode('unicode-escape')
+    clean_text = unicodedata.normalize("NFKD", clean_text)
+    clean_text = re.sub(r'<[^>]*>', '', clean_text)
+    clean_text = re.sub(r'http[s]?://\S+', '', clean_text)
+    clean_text = clean_text.replace("'", "\"")
+
+    return clean_text
+
+
 def do_request(url,data):
     try:
         response = requests.post(url,json=data)
@@ -18,7 +33,7 @@ def do_request(url,data):
 if __name__ == '__main__':
 
     print('Entre com a pergunta : ')
-    data = {'query': input()}
+    data = {'query': normalize(input())}
     url = "http://127.0.0.1:3333/chat/api/bc00edf6-e3f7-4e61-b2b7-93119fa7e669/ask"
   
     response = do_request(url,data)
