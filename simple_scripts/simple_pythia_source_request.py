a93afebd510e125fc3412e6ae3723428ad40ea8b
leandro <alvim.lgm@gmail.com>
2024-06-07 22:44:47 -0300

    add: new source request

---
+++ simple_scripts/simple_pythia_source_request.py
@@ -0,0 +1,58 @@
+import requests
+import simplejson
+
+import unicodedata
+import re
+import codecs 
+
+import os
+from dotenv import dotenv_values
+
+def normalize(text):
+
+    clean_text = text.encode('raw_unicode_escape').decode('unicode-escape')
+    clean_text = unicodedata.normalize("NFKD", clean_text)
+    clean_text = re.sub(r'<[^>]*>', '', clean_text)
+    clean_text = re.sub(r'http[s]?://\S+', '', clean_text)
+    clean_text = clean_text.replace("'", "\"")
+
+    return clean_text
+
+
+def do_request(url,data,headers):
+    try:
+        response = requests.post(url,json=data,headers=headers)
+        print("Status code:", response.status_code)
+        if response.status_code == 200:
+            return response
+        else:
+            print("Request error:", response.status_code)
+            return None
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
+if __name__ == '__main__':
+
+    env_vars = dotenv_values(".env")
+    
+    token  = env_vars["PYTHIA_TOKEN"]
+    uuid   = env_vars["PYTHIA_ANSWER_UUID"]
+ 
+    url = f"http://127.0.0.1:3333/api/messages/{uuid}/sources"
+    print(url)
+    headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json"
+    }
+    response = requests.get(url,headers=headers)
+    if response:
+        caminho_arquivo = "dados.json"
+        data = response.json()
+        with open(caminho_arquivo, "w") as arquivo:
+            simplejson.dump(data, arquivo)
+
+        print("JSON salvo com sucesso em", caminho_arquivo)
+    
+    
